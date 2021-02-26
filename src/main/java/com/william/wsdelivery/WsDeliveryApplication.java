b5640825bee4e54a22cae4ac9e97cfb328ba2d6e
wilfft <68745408+wilfft@users.noreply.github.com>
2021-02-26 13:03:25 -0300

    feat: adicionado classe de segurança

---
+++ src/main/java/com/william/wsdelivery/WsDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.william.wsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/wsdelivery/WsDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.wsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
